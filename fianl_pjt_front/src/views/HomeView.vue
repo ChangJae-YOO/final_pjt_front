e5ea50fe6fd1379ba22c357ca669face9428f064
junwson9 <sjw534237@gmail.com>
2023-05-22 09:59:58 +0900

    feat: 로그아웃 기능 구현

--- fianl_pjt_front/src/views/HomeView.vue
+++ fianl_pjt_front/src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <h1>홈입니다 ㅋㅋ</h1>
+    <div v-if="isLogin">
     <p>Welcome, {{ username }}</p>
+    <button @click="logout">로그아웃</button>
+    </div>
+    <div v-else>
     <router-link :to="{name: 'login'}">로그인 화면</router-link> |
+    </div>
     <router-link :to="{name: 'signup'}">회원가입 화면</router-link> |
     <router-link :to="{name: 'detail'}">디테일화면</router-link> |
     <router-link :to="{name: 'search'}">검색 화면</router-link>
@@ -10,7 +15,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'HomeView',
@@ -23,6 +28,10 @@ export default {
       return this.$store.state.user.username
     },
   },
+  
+  methods: {
+    ...mapActions(['logout']),
+  }
 }
 </script>
 
