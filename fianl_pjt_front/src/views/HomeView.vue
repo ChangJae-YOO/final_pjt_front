fdad2305c10426d3c29aa5fc1e4eee2a3c7dc3c1
junwson9 <sjw534237@gmail.com>
2023-05-22 17:24:51 +0900

    design: navbar추가

--- fianl_pjt_front/src/views/HomeView.vue
+++ fianl_pjt_front/src/views/HomeView.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse ms-4" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item me-4 text-center">
+          <a class="nav-link" aria-current="page" href="/theme">테마 만들기</a>
+        </li>
+        <li class="nav-item me-4 text-center">
+          <a class="nav-link" href="/search">영화 검색</a>
+        </li>
+        <li class="nav-item me-4 text-center">
+          <a class="nav-link" href="#">내가 좋아한 영화</a>
+        </li>
+      </ul>
+      <div v-if="isLogin">
+        <a href="#" @click="logout" class="text-light" style="text-decoration:none">로그아웃</a>
+      </div>
+      <div class="text-light" style="margin-right:24px" v-else>
+      <a href="accounts/login" class="text-light" style="text-decoration:none">로그인</a> /
+      <a href="accounts/signup" class="text-light" style="text-decoration:none">회원가입</a>
+      </div>
+    </div>
+  </div>
+</nav>
     <h1>홈입니다 ㅋㅋ</h1>
-    <div v-if="isLogin">
     <p>Welcome, {{ username }}</p>
-    <button @click="logout">로그아웃</button>
-    </div>
-    <div v-else>
-    <router-link :to="{name: 'login'}">로그인 화면</router-link> |
-    </div>
-    <router-link :to="{name: 'signup'}">회원가입 화면</router-link> |
     <router-link :to="{name: 'detail'}">디테일화면</router-link> |
-    <router-link :to="{name: 'search'}">검색 화면</router-link> |
     <router-link :to="{name: 'theme'}">테마 화면</router-link>
   </div>
 </template>
@@ -37,5 +57,7 @@ export default {
 </script>
 
 <style>
-
+.navbar-brand {
+  padding: 0 30px 0 40px;
+}
 </style>
--- fianl_pjt_front/src/views/SearchView.vue
+++ fianl_pjt_front/src/views/SearchView.vue
@@ -1,8 +1,38 @@
 <template>
   <div>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse ms-4" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item me-4 text-center">
+          <a class="nav-link" aria-current="page" href="/theme">테마 만들기</a>
+        </li>
+        <li class="nav-item me-4 text-center">
+          <a class="nav-link" href="/search">영화 검색</a>
+        </li>
+        <li class="nav-item me-4 text-center">
+          <a class="nav-link" href="#">내가 좋아한 영화</a>
+        </li>
+      </ul>
+      <div v-if="isLogin">
+        <a href="#" @click="logout" class="text-light" style="text-decoration:none">로그아웃</a>
+      </div>
+      <div class="text-light" style="margin-right:24px" v-else>
+      <a href="accounts/login" class="text-light" style="text-decoration:none">로그인</a> /
+      <a href="accounts/signup" class="text-light" style="text-decoration:none">회원가입</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="container">
       <form @submit.prevent="search" class="px-4 py-3">
       <div class="mb-3">
-        <label for="search" class="form-label">검색창</label>
+        <label for="search" class="form-label">
+        </label>
         <input type="text" class="form-control" id="search" placeholder="검색어를 입력하세요" v-model="searchInput">
       </div>
       </form>
@@ -16,12 +46,13 @@
       </div>  
     </div>
   </div>
+</div>
 </template>
 
 <script>
 import CardComponent from '@/components/CardComponent'
 import axios from 'axios'
-import { mapActions } from 'vuex'
+import { mapActions,mapGetters } from 'vuex'
 
 const API_URL = 'http://127.0.0.1:8000'
 
@@ -63,11 +94,12 @@ export default {
         console.log(err)
       })
     },
-    
+    ...mapActions(['logout']),
     ...mapActions(['clearMovies']),
   },
 
   computed: {
+    ...mapGetters(['isLogin']),
     movies(){
       return this.$store.getters.getMovies
     }
@@ -76,5 +108,18 @@ export default {
 </script>
 
 <style>
+.container {
+  padding: 0;
 
+  @media (min-width: 768px) {
+  padding: 0 50px;
+  }
+}
+.container-fluid{
+    padding: 0;
+
+  @media (min-width: 768px) {
+  padding: 0 100px;
+  }
+}
 </style>
